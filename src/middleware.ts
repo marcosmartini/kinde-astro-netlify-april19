45eced8f8e23ecd5ee404458ee326d3cd041246c
Marcos Martini <marcos@kinde.com>
2024-01-31 09:44:45 +1000

    test dynamic var

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,5 @@ export const onRequest = defineMiddleware(async ({ cookies, locals }, next) => {
   locals.accessToken = await session.getSessionItem("access_token");
   locals.refreshToken = await session.getSessionItem("refresh_token");
 
-  console.log(import.meta.url);
-
   return next();
 });
