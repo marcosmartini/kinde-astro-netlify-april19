dbb0379798ec1d642a0696a3be42b7e91081e4bb
Marcos Martini <marcos@kinde.com>
2024-01-31 16:51:00 +1000

    t

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,24 @@ import { createKindeServerClient, GrantType } from "@kinde-oss/kinde-typescript-
 
 import netlify from "@astrojs/netlify";
 
+const isNetlify = process.env.NETLIFY;
+
+const netlifyURLs = {
+  "production": process.env.URL,
+  "deploy-preview": process.env.DEPLOY_URL,
+};
+
+const isNetlifyProd = process.env.CONTEXT == "production"
+
 // Client for authorization code flow
 const kindeClient = createKindeServerClient(GrantType.AUTHORIZATION_CODE, {
   authDomain: process.env.KINDE_ISSUER_URL,
   clientId: process.env.KINDE_CLIENT_ID,
   clientSecret: process.env.KINDE_CLIENT_SECRET,
-  redirectURL: process.env.KINDE_REDIRECT_URL,
-  logoutRedirectURL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+  redirectURL: isNetlify ? `${netlifyURLs[process.env.CONTEXT]}/api/auth/callback` : process.env.KINDE_REDIRECT_URL,
+  logoutRedirectURL: isNetlify
+    ? netlifyURLs[process.env.CONTEXT]
+    :  process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
 });
 
 // https://astro.build/config
