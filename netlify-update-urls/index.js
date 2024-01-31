94c1caf323cc41fbf9b922d0c03e7b39b80ec2d7
Marcos Martini <marcos@kinde.com>
2024-01-31 16:06:30 +1000

    tets

--- netlify-update-urls/index.js
+++ netlify-update-urls/index.js
@@ -2,7 +2,7 @@
 
 export const onPreBuild = async function ({ netlifyConfig }) {
   if (netlifyConfig.build.environment.CONTEXT == "production") {
-    netlifyConfig.build.command = `KINDE_REDIRECT_URL=${netlifyConfig.build.environment.URL} && npm run build`;
+    netlifyConfig.build.command = `KINDE_REDIRECT_URL=$URL npm run build`;
 
     console.log(netlifyConfig);
   }
