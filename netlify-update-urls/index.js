6b68f151a3d21116a53243da95bda5c4e17eb538
Marcos Martini <marcos@kinde.com>
2024-01-31 15:37:15 +1000

    test

--- netlify-update-urls/index.js
+++ netlify-update-urls/index.js
@@ -1,19 +1,7 @@
 // index.js
 
-const updateVars = async (netlifyConfig) => {
-  netlifyConfig.build.environment.KINDE_CLIENT_ID = "e6f800baf7b94b79b8301d87521d4c94";
-  netlifyConfig.build.environment.KINDE_CLIENT_SECRET =
-    "JZAb2v2FstmaFLpSiM0DkesOdnRbUDY3qL7dXSqR5ymLaB3xiTm";
-  netlifyConfig.build.environment.KINDE_ISSUER_URL = "https://mm.kinde.com";
-  netlifyConfig.build.environment.KINDE_POST_LOGOUT_REDIRECT_URL =
-    netlifyConfig.build.environment.URL;
-  netlifyConfig.build.environment.KINDE_REDIRECT_URL = `${netlifyConfig.build.environment.URL}/api/auth/callback`;
-};
-
 export const onPreBuild = async function ({ netlifyConfig }) {
   if (netlifyConfig.build.environment.CONTEXT == "production") {
-    const response = await updateVars(netlifyConfig);
-
-    console.log(response);
+    netlifyConfig.build.command = `KINDE_REDIRECT_URL=$URL npm run build`;
   }
 };
