69ab2bfea791f28db91afd1881e1b4577299322a
Marcos Martini <marcos@kinde.com>
2024-01-31 16:22:52 +1000

    test

--- netlify-update-urls/index.js
+++ netlify-update-urls/index.js
@@ -1,6 +1,6 @@
 // index.js
 
-export const onPreBuild = async function ({ netlifyConfig }) {
+export const onBuild = async function ({ netlifyConfig }) {
   if (netlifyConfig.build.environment.CONTEXT == "production") {
     netlifyConfig.build.environment.KINDE_REDIRECT_URL = `${netlifyConfig.build.environment.URL}/api/auth/callback`;
     netlifyConfig.build.environment.KINDE_POST_LOGOUT_REDIRECT_URL =
