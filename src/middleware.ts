5a3ef117e544083032e752701612a1534e24e4f7
Marcos Martini <marcos@kinde.com>
2024-04-19 14:26:15 +1000

    updated deps, fixed auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,21 @@
 import { defineMiddleware } from "astro:middleware";
 import { sessionManager } from "./lib/sessionManager";
+import kindeClient from "./lib/kinde";
 
-export const onRequest = defineMiddleware(async ({ cookies, locals }, next) => {
+export const onRequest = defineMiddleware(async ({ cookies, locals, url }, next) => {
   const session = sessionManager(cookies);
+  const isAuthenticated = await kindeClient(url.origin).isAuthenticated(sessionManager(cookies));
 
-  locals.user = (await session.getSessionItem("user")) as KindeUser;
-  locals.acStateKey = await session.getSessionItem("ac-state-key");
-  locals.idToken = await session.getSessionItem("id_token");
-  locals.accessToken = await session.getSessionItem("access_token");
-  locals.refreshToken = await session.getSessionItem("refresh_token");
+  if (isAuthenticated) {
+    const user = await kindeClient(url.origin).getUser(sessionManager(cookies));
+    locals.user = user as KindeUser;
+    locals.acStateKey = await session.getSessionItem("ac-state-key");
+    locals.idToken = await session.getSessionItem("id_token");
+    locals.accessToken = await session.getSessionItem("access_token");
+    locals.refreshToken = await session.getSessionItem("refresh_token");
+  }
+
+  // locals.kinde.user = user as KindeUser;
 
   return next();
 });
--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import kindeClient from "../../../lib/kinde";
 import { sessionManager } from "../../../lib/sessionManager";
 
-export const GET: APIRoute = async ({ request, cookies, redirect, url }) => {
+export const GET: APIRoute = async ({ request, cookies, redirect, url, locals }) => {
   await kindeClient(url.origin).handleRedirectToApp(sessionManager(cookies), new URL(request.url));
 
   return redirect("/dashboard", 302);
